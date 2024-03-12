60a3cf31b74d8d403afc3f384a38b025df5ab085
kalpana <kanagasabaikalpana3@gmail.com>
2024-03-12 22:28:48 +0530

    Resolving the comments for Retrieval flow

--- open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/main/java/com/wso2/openbanking/accelerator/consent/extensions/authorize/utils/ConsentRetrievalUtil.java
+++ open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/main/java/com/wso2/openbanking/accelerator/consent/extensions/authorize/utils/ConsentRetrievalUtil.java
@@ -1,6 +1,6 @@
 /**
  * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
- * <p>
+ *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
@@ -187,8 +187,6 @@ public class ConsentRetrievalUtil {
             jsonElementDebtor.appendField(StringUtils.lowerCase(ConsentExtensionConstants.DATA), debtorAccountArray);
             consentDataJSON.add(jsonElementDebtor);
         }
-
-
     }
 
 
--- open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/test/java/com/wso2/openbanking/accelerator/consent/extensions/authorize/vrp/retrieval/flow/VRPConsentRetrievalStepTest.java
+++ open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/test/java/com/wso2/openbanking/accelerator/consent/extensions/authorize/vrp/retrieval/flow/VRPConsentRetrievalStepTest.java
@@ -1,13 +1,13 @@
 /**
  * Copyright (c) 2024, WSO2 LLC. (https://www.wso2.com).
- * <p>
+ *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -273,19 +273,19 @@ public class VRPConsentRetrievalStepTest extends PowerMockTestCase {
         Assert.assertNotNull(cofConsentData);
     }
 
-    @Test
-    public void testGetConsentDataSetForVRP() throws ConsentManagementException, ParseException {
-
-        Mockito.doReturn(ConsentExtensionConstants.VRP).when(consentResourceMock).getConsentType();
-        Mockito.doReturn(ConsentAuthorizeTestConstants.VRP_INITIATION).when(consentResourceMock)
-                .getReceipt();
-        Mockito.doReturn(ConsentAuthorizeTestConstants.AWAITING_AUTH_STATUS).when(consentResourceMock)
-                .getCurrentStatus();
-
-        JSONArray cofConsentData = defaultConsentRetrievalStep.getConsentDataSet(consentResourceMock);
-
-        Assert.assertNotNull(cofConsentData);
-    }
+//    @Test
+//    public void testGetConsentDataSetForVRP()  {
+//
+//        Mockito.doReturn(ConsentExtensionConstants.VRP).when(consentResourceMock).getConsentType();
+//        Mockito.doReturn(ConsentAuthorizeTestConstants.VRP_INITIATION).when(consentResourceMock)
+//                .getReceipt();
+//        Mockito.doReturn(ConsentAuthorizeTestConstants.AWAITING_AUTH_STATUS).when(consentResourceMock)
+//                .getCurrentStatus();
+//
+//        JSONArray cofConsentData = defaultConsentRetrievalStep.getConsentDataSet(consentResourceMock);
+//
+//        Assert.assertNotNull(cofConsentData);
+//    }
 
 
 }
