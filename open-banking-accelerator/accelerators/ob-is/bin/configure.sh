ec7f95c6f2ec143d84e7d6cb72e87e9e61410775
Sandaru-Vithanage <sandaru.rush@gmail.com>
2024-06-27 16:41:25 +0530

    Added Relavant Changes to VRP Initiation

--- open-banking-accelerator/accelerators/ob-is/bin/configure.sh
+++ open-banking-accelerator/accelerators/ob-is/bin/configure.sh
@@ -100,7 +100,6 @@ create_mysql_databases() {
     echo "Database Created: ${DB_IS_CONFIG}"
     mysql -u${DB_USER} ${DB_MYSQL_PASS} -h${DB_HOST} -e "DROP DATABASE IF EXISTS ${DB_OPEN_BANKING_STORE}; CREATE DATABASE ${DB_OPEN_BANKING_STORE};
     ALTER DATABASE ${DB_OPEN_BANKING_STORE} CHARACTER SET latin1 COLLATE latin1_swedish_ci";
-    mysql "ALTER TABLE ${DB_OPEN_BANKING_STORE}.`ob_consent_attribute` CHANGE COLUMN `ATT_VALUE` `ATT_VALUE` VARCHAR(1023) NOT NULL" ;
     echo "Database Created: ${DB_OPEN_BANKING_STORE}"
 };
 
