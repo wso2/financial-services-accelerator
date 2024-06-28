568ef2707581a129c8787365abff5aa6ffd74e1f
Sandaru Vithanage <sandaru.20210334@iit.ac.lk>
2024-06-28 13:54:28 +0530

    Added space line in Periodic Limits class
    Co-authored-by: Ashirwada Dayarathne <ashirwada93@gmail.com>

--- open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/main/java/com/wso2/openbanking/accelerator/consent/extensions/manage/model/PeriodicLimit.java
+++ open-banking-accelerator/components/consent-management/com.wso2.openbanking.accelerator.consent.extensions/src/main/java/com/wso2/openbanking/accelerator/consent/extensions/manage/model/PeriodicLimit.java
@@ -14,6 +14,7 @@ import java.time.temporal.TemporalAdjusters;
  * It includes the period type, amount, period alignment, cyclic expiry time, and cyclic paid amount.
  */
 public class PeriodicLimit {
+    
     private final String periodType;
     private BigDecimal amount;
     private String periodAlignment;
