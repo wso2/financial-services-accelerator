d77d5e899ea6b888fda2e33f54715dd71a920da7
akila94 <akilaaroshanaa@gmail.com>
2024-10-29 11:27:23 +0530

    Overload role validation method to keep consistency

--- open-banking-accelerator/components/com.wso2.openbanking.accelerator.gateway/src/main/java/com/wso2/openbanking/accelerator/gateway/executor/service/CertValidationService.java
+++ open-banking-accelerator/components/com.wso2.openbanking.accelerator.gateway/src/main/java/com/wso2/openbanking/accelerator/gateway/executor/service/CertValidationService.java
@@ -1,19 +1,10 @@
-/**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+/*
+ * Copyright (c) 2021-2023, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content.
  */
 
 package com.wso2.openbanking.accelerator.gateway.executor.service;
@@ -22,6 +13,8 @@ import com.wso2.openbanking.accelerator.common.config.OpenBankingConfigParser;
 import com.wso2.openbanking.accelerator.common.exception.CertificateValidationException;
 import com.wso2.openbanking.accelerator.common.exception.TPPValidationException;
 import com.wso2.openbanking.accelerator.common.model.PSD2RoleEnum;
+import com.wso2.openbanking.accelerator.common.util.eidas.certificate.extractor.CertificateContent;
+import com.wso2.openbanking.accelerator.common.util.eidas.certificate.extractor.CertificateContentExtractor;
 import com.wso2.openbanking.accelerator.gateway.cache.GatewayCacheKey;
 import com.wso2.openbanking.accelerator.gateway.cache.TppValidationCache;
 import com.wso2.openbanking.accelerator.gateway.executor.model.RevocationStatus;
@@ -158,6 +151,54 @@ public class CertValidationService {
         }
     }
 
+    /**
+     * Validates TPP roles with the required roles and the roles inside the certificate.
+     *
+     * @param tppCertificate TPP certificate
+     * @param requiredPSD2Roles required roles
+     * @return true is validation is successful, false otherwise
+     * @throws TPPValidationException TPP validation exception
+     * @throws CertificateValidationException cert validation exception
+     */
+    public boolean validateTppRoles(X509Certificate tppCertificate, List<PSD2RoleEnum> requiredPSD2Roles)
+            throws TPPValidationException, CertificateValidationException {
+
+        if (TPPCertValidatorDataHolder.getInstance().isTppValidationEnabled()) {
+
+            String tppValidationServiceImplClassPath =
+                    TPPCertValidatorDataHolder.getInstance().getTPPValidationServiceImpl();
+            if (StringUtils.isNotBlank(tppValidationServiceImplClassPath)) {
+                TPPValidationService tppValidationService =
+                        TPPCertValidatorDataHolder.getInstance().getTppValidationService();
+
+                if (tppValidationService != null) {
+                    return validateTPP(tppValidationService, tppCertificate, requiredPSD2Roles);
+                } else {
+                    throw new TPPValidationException(
+                            "Unable to find the implementation class for TPP validation service");
+                }
+            } else {
+                throw new TPPValidationException("TPP validation service " +
+                        "class implementation is empty");
+            }
+        } else if (TPPCertValidatorDataHolder.getInstance().isPsd2RoleValidationEnabled()) {
+            return isRequiredRolesMatchWithScopes(tppCertificate, requiredPSD2Roles);
+        } else {
+            throw new TPPValidationException("Both TPP validation and PSD2 role validation services are disabled");
+        }
+    }
+
+
+    /**
+     * Validates TPP roles with the required roles and the roles inside the certificate.
+     *
+     * @param tppCertificate TPP certificate
+     * @param requiredPSD2Roles required roles
+     * @param certRoles roles of the certificate
+     * @return true if the validation is successful, false otherwise
+     * @throws TPPValidationException TPP validation exception
+     * @throws CertificateValidationException cert validation exception
+     */
     public boolean validateTppRoles(X509Certificate tppCertificate, List<PSD2RoleEnum> requiredPSD2Roles,
                                     List<String> certRoles)
             throws TPPValidationException, CertificateValidationException {
@@ -171,26 +212,7 @@ public class CertValidationService {
                         TPPCertValidatorDataHolder.getInstance().getTppValidationService();
 
                 if (tppValidationService != null) {
-
-                    // Initializing certificate cache and cache key
-                    TppValidationCache tppValidationServiceCache = TppValidationCache.getInstance();
-                    String tppValidationCacheKeyStr = tppValidationService.getCacheKey(tppCertificate,
-                            requiredPSD2Roles, Collections.emptyMap());
-                    GatewayCacheKey tppValidationCacheKey = GatewayCacheKey.of(tppValidationCacheKeyStr);
-
-                    // Executing TPP role validation process or retrieve last status from cache
-                    if (tppValidationServiceCache.getFromCache(tppValidationCacheKey) != null) {
-                        // previous result is present in cache, return result
-                        return tppValidationServiceCache.getFromCache(tppValidationCacheKey);
-                    } else {
-                        final boolean result = tppValidationService
-                                .validate(tppCertificate, requiredPSD2Roles, Collections.emptyMap());
-                        if (result) {
-                            // Adding result to cache
-                            tppValidationServiceCache.addToCache(tppValidationCacheKey, true);
-                            return true;
-                        }
-                    }
+                    return validateTPP(tppValidationService, tppCertificate, requiredPSD2Roles);
                 } else {
                     throw new TPPValidationException(
                             "Unable to find the implementation class for TPP validation service");
@@ -204,18 +226,54 @@ public class CertValidationService {
         } else {
             throw new TPPValidationException("Both TPP validation and PSD2 role validation services are disabled");
         }
-        return false;
     }
 
     /**
-     * Validate whether the psd2 roles match with the scopes.
+     * Validate whether the psd2 roles match with the scopes
+     *
+     * @param tppCertificate    eidas certificate with roles
+     * @param requiredPSD2Roles client requested roles
+     * @return true if all required values are present in the certificate
+     */
+    private boolean isRequiredRolesMatchWithScopes(X509Certificate tppCertificate,
+                                                   List<PSD2RoleEnum> requiredPSD2Roles)
+            throws CertificateValidationException, TPPValidationException {
+
+
+        // Extract the certContent from the eidas certificate (i.e. roles, authorization number, etc)
+        CertificateContent certContent = CertificateContentExtractor.extract(tppCertificate);
+
+        if (log.isDebugEnabled()) {
+            log.debug("The TPP is requesting roles: " + requiredPSD2Roles);
+            log.debug("Provided PSD2 eIDAS certificate" +
+                    " contains the role: " + certContent.getPspRoles());
+        }
+
+        // Validate whether the eIDAS certificate contains the required roles that matches with the token scopes.
+        for (PSD2RoleEnum requiredRole : requiredPSD2Roles) {
+            if (!certContent.getPspRoles().contains(requiredRole.name())) {
+                // Return false if any one of the roles that are bound to the scope is not present in the PSD2
+                // role list of the client eIDAS certificate.
+                final String errorMsg = "The PSD2 eIDAS certificate does not contain the required role "
+                        + requiredRole.toString();
+
+                log.error(errorMsg);
+                throw new TPPValidationException(errorMsg);
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Validate whether the psd2 roles match with the scopes
      *
      * @param requiredPSD2Roles client requested roles
      * @return true if all required values are present in the certificate
      */
-    private boolean isRequiredRolesMatchWithScopes(
-            List<PSD2RoleEnum> requiredPSD2Roles, List<String> certRoles)
+    private boolean isRequiredRolesMatchWithScopes(List<PSD2RoleEnum> requiredPSD2Roles, List<String> certRoles)
             throws TPPValidationException {
+
 
         if (log.isDebugEnabled()) {
             log.debug("The TPP is requesting roles: " + requiredPSD2Roles);
@@ -236,5 +294,39 @@ public class CertValidationService {
             }
         }
         return true;
+    }
+
+    /**
+     * Validates the TPP using a custom class implementation which implements TPPValidationService interface.
+     *
+     * @param tppValidationService custom TPP validation service
+     * @param tppCertificate TPP certificate
+     * @param requiredPSD2Roles required TPP roles
+     * @return ture if validation is successful, false otherwise
+     * @throws TPPValidationException TPP validation exception
+     */
+    private boolean validateTPP(TPPValidationService tppValidationService, X509Certificate tppCertificate,
+                                List<PSD2RoleEnum> requiredPSD2Roles) throws TPPValidationException {
+
+        // Initializing certificate cache and cache key
+        TppValidationCache tppValidationServiceCache = TppValidationCache.getInstance();
+        String tppValidationCacheKeyStr = tppValidationService.getCacheKey(tppCertificate,
+                requiredPSD2Roles, Collections.emptyMap());
+        GatewayCacheKey tppValidationCacheKey = GatewayCacheKey.of(tppValidationCacheKeyStr);
+
+        // Executing TPP role validation process or retrieve last status from cache
+        if (tppValidationServiceCache.getFromCache(tppValidationCacheKey) != null) {
+            // previous result is present in cache, return result
+            return tppValidationServiceCache.getFromCache(tppValidationCacheKey);
+        } else {
+            final boolean result = tppValidationService.validate(tppCertificate, requiredPSD2Roles,
+                    Collections.emptyMap());
+            if (result) {
+                // Adding result to cache
+                tppValidationServiceCache.addToCache(tppValidationCacheKey, true);
+                return true;
+            }
+        }
+        return false;
     }
 }
