ddf9d403f57bef791bbed9509a0b0f488af38ee6
ashirwadadayarathne <ashirwada93@gmail.com>
2025-01-07 11:09:04 +0530

    Configuring different super admins for each server

--- financial-services-accelerator/accelerators/fs-apim/bin/configure.ps1
+++ financial-services-accelerator/accelerators/fs-apim/bin/configure.ps1
@@ -141,6 +141,13 @@ Function Set-Hostnames {
     Find-Replace $DEPLOYMENT_TOML_FILE "IS_HOSTNAME" "$( $PROPERTIES.'IS_HOSTNAME' )"
 }
 
+# A function to replace the admin credentials in the temp deployment.toml with their actual values from configure.properties
+Function Set-Admin-Credentials {
+    Find-Replace $DEPLOYMENT_TOML_FILE "APIM_ADMIN_USERNAME" "$( $PROPERTIES.'APIM_ADMIN_USERNAME' )"
+    Find-Replace $DEPLOYMENT_TOML_FILE "APIM_ADMIN_PASSWORD" "$( $PROPERTIES.'APIM_ADMIN_PASSWORD' )"
+    Find-Replace $DEPLOYMENT_TOML_FILE "APIM_ADMIN_NAME" "$( $PROPERTIES.'APIM_ADMIN_NAME' )"
+}
+
 # A utility function to create a database.
 Function Add-Database {
     param ([string]$DB_USER, [string]$DB_PASS, [string]$DB_HOST, [string]$DB_NAME)
@@ -223,6 +230,11 @@ Write-Output "============================================"
 Write-Output "[INFO] Configuring the hostnames..."
 Set-Hostnames
 Write-Output "[INFO] Hostnames configurations completed!"
+
+Write-Output "============================================"
+Write-Output "[INFO] Configuring the admin credentials..."
+Set-Admin-Credentials
+Write-Output "[INFO] Admin credentials configurations completed!"
 
 Write-Output "============================================"
 Write-Output "[INFO] Configuring the datasources..."
