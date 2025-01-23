3d494edb94f009c3fb7c65a8795916ab0e12a13c
vimukthiRajapaksha <reachvimukthi@gmail.com>
2025-01-23 18:04:42 +0530

    Add support to configure custom token type issuer

--- open-banking-accelerator/components/com.wso2.openbanking.accelerator.identity/src/main/java/com/wso2/openbanking/accelerator/identity/listener/application/OBApplicationManagementListener.java
+++ open-banking-accelerator/components/com.wso2.openbanking.accelerator.identity/src/main/java/com/wso2/openbanking/accelerator/identity/listener/application/OBApplicationManagementListener.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com).
+ * Copyright (c) 2023-2025, WSO2 LLC. (https://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -108,6 +108,7 @@ public class OBApplicationManagementListener extends AbstractApplicationMgtListe
 
             OAuthConsumerAppDTO oAuthConsumerAppDTO = oAuthAdminService
                     .getOAuthApplicationDataByAppName(serviceProvider.getApplicationName());
+
             LocalAndOutboundAuthenticationConfig localAndOutboundAuthenticationConfig = serviceProvider
                     .getLocalAndOutBoundAuthenticationConfig();
 
