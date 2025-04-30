25c0835293a0b127f7857174b3174f7f3900abb9
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-04-30 12:07:23 +0530

    updated workflow with test-automation folder

--- test-automation/test.sh
+++ test-automation/test.sh
@@ -80,7 +80,8 @@ sudo mkdir -p /home/$name/.wso2-updates/docker && sudo chmod -R 777 /home/$name/
 
 
 cp ${RUNNER_HOME}/test-automation/wso2update_linux $TEST_HOME/wso2is-7.0.0/bin/
-$TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $WSO2_USERNAME --password $WSO2_PASSWORD ||  ($TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $WSO2_USERNAME --password $WSO2_PASSWORD )
+sudo $TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $WSO2_USERNAME --password $WSO2_PASSWORD ||
+($TEST_HOME/wso2is-7.0.0/bin/wso2update_linux --username $WSO2_USERNAME --password $WSO2_PASSWORD )
 
 printf "%s\n%s\n" "$WSO2_USERNAME" "$WSO2_PASSWORD" | $TEST_HOME/wso2is-7.0.0/bin/wso2update_linux
 
