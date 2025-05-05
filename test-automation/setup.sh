9e79a22c4c40808d6be6603b3c044010cba76465
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-05-05 13:48:29 +0530

    added env

--- test-automation/setup.sh
+++ test-automation/setup.sh
@@ -86,7 +86,7 @@ mvn -B install --file ${RUNNER_HOME}/pom.xml
 MVNSTATE=$?
 #
 echo '======================= Moving Packs to RUNNER_HOME ======================='
-unzip ${RUNNER_HOME}/accelerators/fs-is/target/wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
+unzip financial-services-accelerator/accelerators/fs-is/target/wso2-fsiam-accelerator-4.0.0-M3.zip -d $TEST_HOME/wso2is-7.0.0/
 
 echo '======================= Setup MYSQL ======================='
 sudo apt-get update
