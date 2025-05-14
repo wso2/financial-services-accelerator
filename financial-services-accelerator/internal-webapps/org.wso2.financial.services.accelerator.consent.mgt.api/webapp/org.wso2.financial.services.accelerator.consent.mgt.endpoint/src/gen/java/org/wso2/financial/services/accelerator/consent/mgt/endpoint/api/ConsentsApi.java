c46ccb1cd88a7ef8ab3d1f3157a21b0d32bafa12
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-05-14 09:38:04 +0530

    fixed spot bug issues

--- financial-services-accelerator/internal-webapps/org.wso2.financial.services.accelerator.consent.mgt.api/webapp/org.wso2.financial.services.accelerator.consent.mgt.endpoint/src/gen/java/org/wso2/financial/services/accelerator/consent/mgt/endpoint/api/ConsentsApi.java
+++ financial-services-accelerator/internal-webapps/org.wso2.financial.services.accelerator.consent.mgt.api/webapp/org.wso2.financial.services.accelerator.consent.mgt.endpoint/src/gen/java/org/wso2/financial/services/accelerator/consent/mgt/endpoint/api/ConsentsApi.java
@@ -43,7 +43,7 @@ import javax.ws.rs.core.Response;
 // control
 @Path("/consents")
     @Api(description = "the consents API")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2025-05-14T09:16:51.275283650+05:30[Asia/Colombo]", comments = "Generator version: 7.12.0")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2025-05-14T09:36:11.609583+05:30[Asia/Colombo]", comments = "Generator version: 7.12.0")
 public class ConsentsApi {
 ConsentAPIImpl consentApiImpl = new ConsentAPIImpl();
 
