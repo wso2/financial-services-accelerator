1b62d60a39a114945104220de26fabec8d8e1fa3
ParameswaranSajeenthiran <psajeendran@gmail.com>
2025-05-18 21:13:52 +0530

    restrucutred folder

---
+++ financial-services-accelerator/consent-mgt-api/org.wso2.financial.services.accelerator.consent.mgt.endpoint/src/main/java/org/wso2/financial/services/accelerator/consent/mgt/endpoint/constants/ConsentConstant.java
@@ -0,0 +1,15 @@
+package org.wso2.financial.services.accelerator.consent.mgt.endpoint.constants;
+
+
+/**
+ * Constant class for consent extension module.
+ */
+public class ConsentConstant {
+
+    // error codes
+    public static final String PAYLOAD_SCHEMA_VALIDATION_ERROR = "CMGT_10000";
+
+    public static final String PAYLOAD_SCHEMA_VALIDATION_ERROR_MESSAGE = "Invalid request payload.";
+    public static final String PAYLOAD_SCHEMA_VALIDATION_ERROR_DESCRIPTION =
+            "Please ensure the JSON structure matches the expected schema. ";
+}
