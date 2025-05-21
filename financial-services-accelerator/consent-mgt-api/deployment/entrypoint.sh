61025a3f2adea4eff743565d9ecfe4e84a370449
Parameswaran Sajeenthiran <77486691+ParameswaranSajeenthiran@users.noreply.github.com>
2025-05-21 15:21:08 +0530

    [5.0.0-consent] DAO layer implementation of Consent Management API  (#599)
    DAO layer implementation of Consent Management API

---
+++ financial-services-accelerator/consent-mgt-api/deployment/entrypoint.sh
@@ -0,0 +1,31 @@
+# Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+#
+# WSO2 LLC. licenses this file to you under the Apache License,
+# Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+#!/bin/bash
+set -e
+export TRIVY_DISABLE_VEX_NOTICE=true
+
+# Ensure /usr/local/tomcat/webapps exists
+mkdir -p /usr/local/tomcat/webapps
+
+# Copy the WAR file as ROOT.war if it doesn't already exist
+if [ ! -f "/usr/local/tomcat/webapps/ROOT.war" ]; then
+    echo "Deploying ROOT.war..."
+    cp /usr/local/tomcat/consent.war /usr/local/tomcat/webapps/ROOT.war
+fi
+
+# Start Tomcat
+exec /usr/local/tomcat/bin/catalina.sh run
---
+++ financial-services-accelerator/consent-mgt-api/org.wso2.financial.services.accelerator.consent.mgt.api.dao/src/main/java/org/wso2/financial/services/accelerator/consent/mgt/api/dao/exceptions/ConsentMgtException.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2025, WSO2 LLC. (https://www.wso2.com).
+ * <p>
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.financial.services.accelerator.consent.mgt.api.dao.exceptions;
+
+import org.wso2.financial.services.accelerator.consent.mgt.api.dao.constants.ConsentError;
+
+/**
+ * Used for creating runtime exceptions for financial services modules.
+ */
+public class ConsentMgtException extends Exception {
+
+    private static final long serialVersionUID = -5686395831712095972L;
+    private ConsentError error;
+
+    public ConsentMgtException(String message) {
+
+        super(message);
+    }
+
+    public ConsentMgtException(ConsentError e) {
+        this.error = e;
+    }
+
+    public ConsentMgtException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConsentError getError() {
+
+        return error;
+    }
+}
