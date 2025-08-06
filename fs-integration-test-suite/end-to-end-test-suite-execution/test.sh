e5f03e2356d4fef6b71617da643a77bc0cab66b8
malshanisenarathne <malshani@wso2.com>
2025-08-06 10:20:11 +0530

    Change wait time

--- fs-integration-test-suite/end-to-end-test-suite-execution/test.sh
+++ fs-integration-test-suite/end-to-end-test-suite-execution/test.sh
@@ -150,7 +150,7 @@ cd ${ACCELERATOR_TESTS_HOME}/accelerator-tests/preconfiguration.steps
 mvn clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 MVNSTATE=$?
 
-sleep 3h
+sleep 2h
 
 echo "-----------------Executing IS tests----------------"
 cd ${IS_TEST_HOME}/dcr
