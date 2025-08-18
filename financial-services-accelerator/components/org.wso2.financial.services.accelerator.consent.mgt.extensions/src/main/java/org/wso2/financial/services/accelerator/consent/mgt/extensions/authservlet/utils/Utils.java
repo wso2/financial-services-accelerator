0832691d50e78437f4f762f5ffa8d7a5156937c8
ashirwadadayarathne <ashirwada93@gmail.com>
2025-08-18 13:48:06 +0530

    Adding log improvements

--- financial-services-accelerator/components/org.wso2.financial.services.accelerator.consent.mgt.extensions/src/main/java/org/wso2/financial/services/accelerator/consent/mgt/extensions/authservlet/utils/Utils.java
+++ financial-services-accelerator/components/org.wso2.financial.services.accelerator.consent.mgt.extensions/src/main/java/org/wso2/financial/services/accelerator/consent/mgt/extensions/authservlet/utils/Utils.java
@@ -335,10 +335,7 @@ public class Utils {
      * @return  fetched language property (with fallback)
      */
     public static String[] getLanguagePropertiesForLocale(Locale locale) {
-        if (log.isDebugEnabled()) {
-            log.debug(String.format("Getting language properties for locale: %s",
-                    locale.toString().replaceAll("[\n\r]", "")));
-        }
+        log.debug("Getting language properties for locale");
         try (InputStream inputStream = getClassLoaderResourceAsStream("LanguageOptions.properties")) {
 
             if (inputStream == null) {
