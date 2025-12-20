7ef98e3be470c090787a5240d0dfdcf03c729a14
Imesh Perera <imeshu@wso2.com>
2025-12-20 14:27:42 +0530

    Remove extra brackets

--- financial-services-accelerator/components/org.wso2.financial.services.accelerator.identity.extensions/src/main/java/org/wso2/financial/services/accelerator/identity/extensions/util/IdentityCommonUtils.java
+++ financial-services-accelerator/components/org.wso2.financial.services.accelerator.identity.extensions/src/main/java/org/wso2/financial/services/accelerator/identity/extensions/util/IdentityCommonUtils.java
@@ -379,8 +379,8 @@ public class IdentityCommonUtils {
                 return Boolean.parseBoolean(regulatoryProperty.toString());
             } else {
                 if (log.isDebugEnabled()) {
-                    log.debug(("Regulatory property not found in service provider metadata. " +
-                            "Hence treating it as a non-regulatory application."));
+                    log.debug("Regulatory property not found in service provider metadata. "
+                            + "Hence treating it as a non-regulatory application.");
                 }
                 return false;
             }
